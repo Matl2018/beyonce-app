d3be5530aab8299a38df5e0c8d1d124cf77ab21d
matl2018 <lumierematerne11@gmail.com>
2018-05-26 15:34:50 +0100

    Added the list of albums with JSON.stringfy

--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
-import propTypes from 'prop-types';
+import React, { Component } from "react";
+import propTypes from "prop-types";
 
 class Albums extends Component {
-  render() {
-    return (
-      <div></div>
+  constructor() {
+    super();
+    this.state = {
+      albums: []
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://rawgit.com/rarmatei/f5ae92ac93d9716affab822a3f54f95b/raw/e62641b3f5ddd12c4fe34aa0912488224594e5a7/beyonce-albums.json"
     )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          albums: data
+        });
+      });
+  }
+
+  render() {
+    return <div>{JSON.stringify(this.state.albums)} </div>;
   }
 }
 
-Albums.propTypes = {
-
-};
+Albums.propTypes = {};
 
 export default Albums;
